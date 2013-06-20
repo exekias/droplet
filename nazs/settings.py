dfe237cbd0b53c225bbedbaeabdc30875ea982f2
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-06-20 14:00:38 +0200

    use django nose test runner

--- nazs/settings.py
+++ nazs/settings.py
@@ -112,8 +112,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django_nose',
     'nazs.core',
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
