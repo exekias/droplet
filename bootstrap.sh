5f4ed68e100bcb148fd4a7ac5d72cc9a7e2f18e1
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-06-25 20:33:52 +0200

    added bootstrap script
    it will create a virtual env and install requirements

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Creating virtualenv under env dir..."
+virtualenv env
+
+echo "Activating virtual env"
+. env/bin/activate
+
+echo "Installing pip dependencies"
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+
