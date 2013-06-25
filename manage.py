4bf19b4fe447a8f0b3756ca955ab2b1c585da2eb
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-06-25 20:34:16 +0200

    auto activate virtual env in manage.py script

--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 import os
-from django.core import management
+import sys
 
+# Include . in python path
+sys.path.append('.')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nazs.settings'
+
+# Activate virtual env
+manage_path = os.path.dirname(os.path.realpath(__file__))
+activate_this = os.path.join(manage_path, 'env/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
+from django.core import management
 
 if __name__ == "__main__":
     management.execute_from_command_line()
