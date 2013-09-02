eb9f02a826ae10f20b4abeef71dfcbf21f1ced19
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-09-02 22:06:58 +0200

    added logging to sudo module

--- nazs/core/sudo.py
+++ nazs/core/sudo.py
@@ -3,6 +3,9 @@ from contextlib import contextmanager
 
 import os
 import pwd
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def set_euid():
@@ -15,7 +18,18 @@ def set_euid():
     See root context manager
     """
     uid = int(pwd.getpwnam(settings.RUN_AS_USER).pw_uid)
-    os.seteuid(uid)
+    logger.debug('Current EUID is %s' % os.getuid())
+    if uid != os.geteuid():
+        try:
+            os.seteuid(uid)
+            logger.info('Set EUID to %s' % settings.RUN_AS_USER)
+        except:
+            current_user = pwd.getpwuid(os.getuid()).pw_name
+            logger.error("Failed to set '%s' EUID, running as '%s'" %
+                         (settings.RUN_AS_USER, current_user))
+            raise e
+    else:
+        logger.debug('Didn\'t set EUID, it was already correct')
 
 
 def drop_privileges():
@@ -41,6 +55,8 @@ def root():
         ...
 
     """
+    logger.info('Entering ROOT mode')
     os.seteuid(0)
     yield
-    set_euid()
+    os.seteuid(int(pwd.getpwnam(settings.RUN_AS_USER).pw_uid))
+    logger.info('Exited ROOT mode')
