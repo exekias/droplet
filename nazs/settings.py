90e4b1b3906d31793a8820dfce0de508460079e5
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2013-09-02 22:03:45 +0200

    configured logging

--- nazs/settings.py
+++ nazs/settings.py
@@ -78,26 +78,30 @@ INSTALLED_APPS = (
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
+# TODO log to file
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s [%(levelname)s] %(module)s - %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'nazs': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
             'propagate': True,
-        },
+        }
     }
 }
