42eeec0bc228b61cafa6dc09f276300d66229610
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-04-29 01:10:04 +0200

    Add samba wizard example (WIP)

---
+++ nazs/samba/blocks.py
@@ -0,0 +1,22 @@
+from achilles import blocks, forms
+from nazs.core.wizards import InstallWizard
+
+from .forms import Mode
+from .module import Samba
+
+
+register = blocks.Library('samba')
+
+
+@register.block
+class mode(forms.Form):
+    form_class = Mode
+
+    def form_valid(self, request, form):
+        pass
+
+
+@register.block(name='install')
+class InstallSamba(InstallWizard):
+    module = Samba
+    forms = [mode, ]
