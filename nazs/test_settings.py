ac1263261156bfa4e2de020f3ee72871f60fb1f1
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-06-23 23:59:51 +0200

    Clean tests output

--- nazs/test_settings.py
+++ nazs/test_settings.py
@@ -41,7 +41,7 @@ LOGGING = {
     },
     'loggers': {
         '': {
-            'handlers': ['console'],
+            'handlers': ['null'],
             'level': 'DEBUG',
             'propagate': True,
         },
