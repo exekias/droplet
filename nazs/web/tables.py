1a460ca8b6b3899a905360bb6bfb0e8548fd6606
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-07-24 00:53:22 +0200

    Allow table items adding trough AddAction

--- nazs/web/tables.py
+++ nazs/web/tables.py
@@ -1,7 +1,7 @@
 from achilles.tables import *  # noqa
 
 
-# Edit Column
+# OBJECT EDIT COLUMN
 
 class EditColumn(ButtonColumn):
     """
@@ -18,7 +18,7 @@ class EditColumn(ButtonColumn):
                (self.table.register_name, self.form, obj.id)
 
 
-# Global Table Actions
+# TABLE ACTIONS
 
 class TableAction(object):
     """
@@ -26,17 +26,21 @@ class TableAction(object):
     """
     creation_counter = 0
 
-    def __init__(self, verbose_name=None,
-                 visible=lambda x: True):
+    def __init__(self, action, verbose_name=None, visible=lambda x: True,
+                 classes='btn btn-default'):
         """
         :param verbose_name: Column human-readable name
         :param accessor: Function to access get data from the object
         :param visible: Function giving visibility flag for the given row
+        :param action: Action to call
+        :param classes: CSS classes to apply to the button
         """
         TableAction.creation_counter += 1
         self.creation_counter = TableAction.creation_counter
         self.verbose_name = verbose_name
         self.visible = visible
+        self.action = action
+        self.classes = classes
 
     def contribute_to_class(self, table, name):
         self.name = name
@@ -52,8 +56,34 @@ class TableAction(object):
         else:
             return ''
 
+    def get_href(self):
+        return ("javascript:achilles.action('web:table_action',"
+                "['%s', '%s'])") % (self.table.register_name, self.name)
+
     def content(self):
-        return self.verbose_name
+        return '<a href="%s" class="%s">%s</a>' % \
+               (self.get_href(), self.classes, self.verbose_name)
+
+    def call(self, *args, **kwargs):
+        return self.action(*args, **kwargs)
+
+
+class AddAction(TableAction):
+    """
+    Table object add action, show the given form to create a new item
+    in the table
+    """
+    def __init__(self, form_name, *args, **kwargs):
+        super(AddAction, self).__init__(None, *args, **kwargs)
+        self.form_name = form_name
+
+    def get_href(self):
+        return ("javascript:achilles.loadInto(achilles.block('%s')"
+                ".find('.pretable'), '%s')") % \
+               (self.table.register_name, self.form_name)
+
+
+# TABLE CLASS OVERRIDES
 
 
 class Table(Table):
