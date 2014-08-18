937c7d36eb47e232b0411da31855059c5d0a0daa
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-08-18 02:40:20 +0200

    Add nazs [save|start|stop|restart] commands

---
+++ nazs/management/commands/restart.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from nazs import modules
+
+
+class Command(BaseCommand):
+    help = 'Restart module daemons'
+
+    def handle(self, *args, **kwargs):
+        names = [n.upper() for n in args]
+        for module in modules():
+            if module.name.upper() in names:
+                if module.enabled:
+                    module.restart()
+                    names.remove(module.name.upper())
+
+                else:
+                    raise CommandError('Module %s is disabled' % module.name)
+
+        if names:
+            raise CommandError('Module %s does not exist' % ','.join(names))
