45e8a2c468059a6cbd29277f88de40fc9f3057e5
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-08-19 20:11:10 +0200

    Update packaging and LICENSE

--- setup.py
+++ setup.py
@@ -7,11 +7,31 @@ from nazs import __version__
 setup(
     name='nazs',
     version=__version__,
-    author='NAZS Team',
+    url='https://github.com/exekias/nazs',
+    author='NAZS team',
+    license='AGPLv3',
+    description='',
+    long_description=open('README.md').read(),
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     scripts=[
         'bin/nazs'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
     ],
 )
