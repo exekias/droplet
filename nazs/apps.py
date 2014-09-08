0701e34c76a4ea55b1334c9b48c88fd346f49fa2
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2014-09-09 00:11:19 +0200

    Stop auto creation of shm database

--- nazs/apps.py
+++ nazs/apps.py
@@ -18,7 +18,6 @@
 
 from django.apps import AppConfig
 
-import os
 import pkg_resources
 
 
@@ -26,19 +25,8 @@ class NAZSConfig(AppConfig):
     name = 'nazs'
 
     def ready(self):
-        from django.core import management
-        from django.conf import settings
-
         from .sudo import set_euid
         set_euid()
-
-        # Sync volatile db and set permissions
-        volatile_db = settings.DATABASES['volatile']['NAME']
-        management.call_command('syncdb',
-                                database='volatile',
-                                interactive=False,
-                                verbosity=0)
-        os.chmod(volatile_db, 0600)
 
         # Load all modules
         for app in pkg_resources.iter_entry_points('nazs.app'):
